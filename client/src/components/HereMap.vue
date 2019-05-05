f22fd8dd5ec1ac0eed3d0ddbd613e9c9f6b185ed
alex <alexnguyen98@yahoo.com>
2019-05-05 09:46:53 +0200

    neco

--- client/src/components/HereMap.vue
+++ client/src/components/HereMap.vue
@@ -77,8 +77,6 @@ export default {
         this.addBubble,
         false
       );
-
-      this.updatePosition();
     },
     loadSVG(url) {
       fetch('/container-blank.svg').then(data => data.text()).then(text => {this.svgMarker = new H.map.Icon(text);console.log('created svg marker')});
@@ -114,55 +112,49 @@ export default {
         console.log(cord);
         this.curPos = {lat:cord.coords.latitude,lng:cord.coords.longitude};
         if (me) { this.map.removeObject(me) }
+        let svgMarkup ='<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path d="M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zm0-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
+        let icon = new H.map.Icon(svgMarkup);
+        let coords = this.curPos;
+        me = new H.map.Marker(coords, { icon: this.svgLocationMarker });
+        this.map.addObject(me);
         if (this.show && this.firstTime) {
-          let svgMarkup =
-            '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path d="M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zm0-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
-          let icon = new H.map.Icon(svgMarkup);
-          let coords = this.curPos;
-          // if (me) {
-          //   this.map.removeObject(me);
-          // }
-          me = new H.map.Marker(coords, { icon: this.svgLocationMarker });
-          this.map.addObject(me);
-          if (this.firstTime) {
-            this.axios({
-              url: "http://localhost:5000/get-containers",
-              method: "get",
-              params: {
-                lat: this.curPos.lat,
-                lng: this.curPos.lng,
-                containers: [1].join(",")
+          this.axios({
+            url: "http://localhost:5000/get-containers",
+            method: "get",
+            params: {
+              lat: this.curPos.lat,
+              lng: this.curPos.lng,
+              containers: [1].join(",")
+            }
+          })
+            .then(data => {
+              let containers = data.data.data;
+              // console.log(containers);
+              for (let i = 0; i < containers.length; i++) {
+                if (containers[i].public) {
+                  // let marker = new H.map.Marker({lat:containers[i].coordinates[0], lng:containers[i].coordinates[1]});
+                  if(containers[i].public) {
+                    
+                  this.addToGroup(
+                    {
+                      lat: containers[i].coordinates[0],
+                      lng: containers[i].coordinates[1]
+                    },
+                    generateMarkerBlob(containers[i]),
+                  );
+                  } 
+                }
               }
             })
-              .then(data => {
-                let containers = data.data.data;
-                // console.log(containers);
-                for (let i = 0; i < containers.length; i++) {
-                  if (containers[i].public) {
-                    // let marker = new H.map.Marker({lat:containers[i].coordinates[0], lng:containers[i].coordinates[1]});
-                    if(containers[i].public) {
-                      
-                    this.addToGroup(
-                      {
-                        lat: containers[i].coordinates[0],
-                        lng: containers[i].coordinates[1]
-                      },
-                      generateMarkerBlob(containers[i]),
-                    );
-                    } 
-                  }
-                }
-              })
-              .catch(err => {
-                console.log("error occured", err);
-              });
-            this.map.setCenter({
-              lat: cord.coords.latitude,
-              lng: cord.coords.longitude
+            .catch(err => {
+              console.log("error occured", err);
             });
-            this.map.setZoom(17);
-            this.firstTime = false;
-          }
+          this.map.setCenter({
+            lat: cord.coords.latitude,
+            lng: cord.coords.longitude
+          });
+          this.map.setZoom(17);
+          this.firstTime = false;
         }},
         err => {
           console.log(err);
